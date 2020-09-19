84d08f27bec6f75716dcddc5b58246d0e7e2431d
yuri <gardinazzi.yuri@gmail.com>
2020-09-19 16:25:27 +0200

    change add item form

--- product_management/forms.py
+++ product_management/forms.py
@@ -4,18 +4,10 @@ from django import forms
 from .models import Product
 
 
-class SubmitButtonMixin(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.id:
-            self.helper.inputs[0].value = 'Update'
-            self.helper.inputs[0].field_classes = 'btn btn-warning'
-        else:
-            self.helper.inputs[0].value = 'Create'
-            self.helper.inputs[0].field_classes = 'btn btn-success'
 
 
-class ProductCrispyForm(SubmitButtonMixin):
+
+class ProductCrispyForm(forms.ModelForm):
 
     helper = FormHelper()
     helper.form_id = 'product-crispy-form'
