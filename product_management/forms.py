d790b3236289584a9ba0ba5ac3472f7000de141c
yuri <gardinazzi.yuri@gmail.com>
2020-09-19 21:05:12 +0200

    personalized Category form field

--- product_management/forms.py
+++ product_management/forms.py
@@ -1,13 +1,20 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django import forms
-from .models import Product
+from django.forms import ModelChoiceField
+
+from .models import Product, MyCategory
 
 
+# Personalized field that show just the name of the object Category
+class CategoryChoiceField(ModelChoiceField):
 
+    def label_from_instance(self, obj):
+        return '{name}'.format(name=obj.name)
 
 
 class ProductCrispyForm(forms.ModelForm):
+    category = CategoryChoiceField(queryset=MyCategory.objects.all())
 
     helper = FormHelper()
     helper.form_id = 'product-crispy-form'
