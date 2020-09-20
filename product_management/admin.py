4104350d5e2de47f0fd9b2d2f2dcd098bba386e1
yuri <gardinazzi.yuri@gmail.com>
2020-09-20 14:55:56 +0200

    removed useless fields

--- product_management/admin.py
+++ product_management/admin.py
@@ -5,7 +5,7 @@ from .models import Product, MyCategory
 
 # Register your models here.
 class CustomProductDisplay(admin.ModelAdmin):
-    list_display = ('producer', 'name', 'price', 'discount', 'category')
+    list_display = ('producer', 'name', 'price', 'category')
     search_field = ('producer', 'name')
     # readonly_fields = ('date_joined', 'last_login')
     filter_horizontal = ()
