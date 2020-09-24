94bcdcc4d3e2263104a0cba6e7d0efefed948f2a
yuri <gardinazzi.yuri@gmail.com>
2020-09-24 20:24:20 +0200

    add recommendation system

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,6 +2,10 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from user_management.models import CustomUser
 from product_management.models import MyCategory, Product
+from sales.models import Purchase
+from numpy import dot
+from numpy.linalg import norm
+import numpy as np
 
 
 def home(request):
@@ -35,12 +39,106 @@ def get_search(request):
             print("qualche categoria, query presente")
             products = Product.objects.filter(name__contains=query, category_id=category)
             context['products'] = products
+    suggested_items = get_similary_items(request.user.id)
+    items = []
+    if(suggested_items):
+
+        for element in suggested_items:
+            items.append(Product.objects.get(id=element))
+    context['suggested_items'] = items
     return render(request, 'search.html', context)
 
 
 def get_categories(request):
-    data  = {'success' : False}
+    data = {'success': False}
     if request.method == 'POST':
         categories = MyCategory.objects.all().values()
         data = {'categories': list(categories)}
     return JsonResponse(data, safe=False)
+
+
+
+
+
+def jaccard(im1, im2):
+    """
+    Computes the Jaccard metric, a measure of set similarity.
+    Parameters
+    ----------
+    im1 : array-like, bool
+        Any array of arbitrary size. If not boolean, will be converted.
+    im2 : array-like, bool
+        Any other array of identical size. If not boolean, will be converted.
+    Returns
+    -------
+    jaccard : float
+        Jaccard metric returned is a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+
+    Notes
+    -----
+    The order of inputs for `jaccard` is irrelevant. The result will be
+    identical if `im1` and `im2` are switched.
+    """
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    intersection = np.logical_and(im1, im2)
+
+    union = np.logical_or(im1, im2)
+
+    return intersection.sum() / float(union.sum())
+
+
+def get_similary_items(id):
+    data = {}
+    list = CustomUser.objects.exclude(id=id)
+    users = list.values('id')
+    products = Product.objects.values('id')
+
+    dim_users = len(users)
+    dim_products = len(products)
+    matrix = np.zeros((dim_users, dim_products))
+    input_user_vector = np.zeros((1, dim_products))
+
+    #set the boolean matrix
+    for i in range(dim_users):
+        for j in range(dim_products):
+            purchase = Purchase.objects.filter(buyer_id=users[i]['id'], product_id=products[j]['id'])
+            if purchase:
+                matrix[i][j] = 1.0
+            else:
+                matrix[i][j] = 0.0
+
+    #set the boolean user vector
+    for i in range(dim_products):
+        purchase = Purchase.objects.filter(buyer_id=id, product_id=products[i]['id'])
+        if purchase:
+            input_user_vector[0][i] = 1.0
+        else:
+            input_user_vector[0][i] = 0.0
+
+    max = 0.0
+    pos = 0
+    list_id_to_retrieve = []
+
+    #Get the Row with the highest jaccard coeff
+    for i in range(dim_users):
+        jacc_coeff = jaccard(matrix[i][:], input_user_vector[0][:])
+        if (jacc_coeff > max):
+            pos = i
+            max = jacc_coeff
+
+    #add to the list the items that are present in the row with the highest jaccard coeff
+    #that the user never bought
+    for i in range(dim_products):
+        if((matrix[pos][i]-input_user_vector[0][i]) == 1):
+            list_id_to_retrieve.append(products[i]['id'])
+
+    return list_id_to_retrieve
+
+
--- product_management/models.py
+++ product_management/models.py
@@ -1,8 +1,6 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from user_management.models import CustomUser
-
-
 # Create your models here.
 class MyCategory(models.Model):
     name = models.CharField(max_length=20, primary_key=True)
@@ -23,9 +21,13 @@ class Product(models.Model):
     producer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     category = models.ForeignKey(MyCategory,on_delete=models.PROTECT)
 
+
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
     review = models.TextField(max_length=400)
     rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(0)])
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'review'
