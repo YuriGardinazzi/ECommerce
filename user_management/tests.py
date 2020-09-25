3b66a18c6e5bbbda8cd0ec5a3bb6c1c9cc466580
yuri <gardinazzi.yuri@gmail.com>
2020-09-25 11:58:15 +0200

    another test

--- user_management/tests.py
+++ user_management/tests.py
@@ -31,3 +31,14 @@ class TestSignUpView(TestCase):
         self.login_user()
         response = self.client.get(reverse_lazy('product_management:product_management'))
         self.assertEqual(response.status_code,200,"user should be logged in")
+
+    def test_user_account_view_logged_in(self):
+        self.login_user()
+        response = self.client.get(reverse_lazy('user_management:account'))
+        self.assertEqual(response.status_code,200,'user logged in')
+
+    def test_user_account_view_not_logged_in(self):
+        response = self.client.get(reverse_lazy('user_management:account'))
+        self.assertEqual(response.status_code,302,'User not logged in should be redirected')
+
+    
